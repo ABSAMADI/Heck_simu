138842702b7e67174b9d4fc4cd8eafcd0f7d927c
ABOLFAZL <a13732673@gmail.com>
2025-05-13 22:36:47 +0330

    Add files via upload

---
+++ script.py
@@ -0,0 +1,65 @@
+try:
+            import random
+            import colorama
+            import cowsay
+            import time
+            
+            colorama.init()
+            
+            print(colorama.Fore.GREEN)
+            cowsay.tux("hello im a abolfazl me github is @ABSAMADI")
+            print(colorama.Fore.RESET)
+            print(colorama.Fore.GREEN + '1.green ' + colorama.Fore.RED + '2.red ' + colorama.Fore.YELLOW + '3.yellow ' + colorama.Fore.BLUE + '4.blue ' + colorama.Fore.MAGENTA + '5.magenta ' + colorama.Fore.CYAN + '6.cyan ' + colorama.Fore.WHITE + '7.All colors not gey' + colorama.Fore.BLACK + " 8.no gey")
+            try:
+                a = int(input(colorama.Fore.LIGHTGREEN_EX + "Enter a number: "))
+            except:
+                print("error: enter a number")
+
+            if a == 1:
+                while 1:
+                    print(colorama.Fore.GREEN + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+
+            if a == 2:
+                while 1:
+                    print(colorama.Fore.RED + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+
+            if a == 3:
+                while 1:
+                    print(colorama.Fore.YELLOW + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+
+            if a == 4:
+                while 1:
+                    print(colorama.Fore.BLUE + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+
+            if a == 5:
+                while 1:
+                    print(colorama.Fore.MAGENTA + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+
+
+            if a == 6:
+                while 1:
+                    print(colorama.Fore.CYAN + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+
+
+            if a == 7:
+                while 1:
+                    print(colorama.Fore.GREEN + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+                    print(colorama.Fore.RED + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+                    print(colorama.Fore.YELLOW + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+                    print(colorama.Fore.BLUE + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+                    print(colorama.Fore.MAGENTA + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+                    print(colorama.Fore.CYAN + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+
+            if a == 8:
+                while 1:
+                    print(colorama.Fore.BLACK + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)   
+                    print(colorama.Fore.WHITE + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)               
+
+
+except:
+    time.sleep(1)
+    print(colorama.Fore.BLUE + "github @ABSAMADI")
+    time.sleep(1)
+    print(colorama.Fore.GREEN + "Good boy")
+    time.sleep(1)
+    print(colorama.Fore.RED + "end")
