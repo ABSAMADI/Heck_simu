5da79c80090c254396033a5dc5a251043fee60ab
ABOLFAZL <a13732673@gmail.com>
2025-08-09 21:34:19 +0330

    Update script.py

--- script.py
+++ script.py
@@ -3,103 +3,134 @@ try:
             import colorama
             import cowsay
             import time
+            import os
             
             colorama.init()
+
+            print(colorama.Fore.RED + "000000000000000000000000000000       000000            000000                  00000^00000")
+            print(colorama.Fore.RED + "000000000000000000000000000000       000000            000000                 000000 000000")
+            print(colorama.Fore.RED + "000000000000000000000000000000       000000            000000                000000   000000")
+            print(colorama.Fore.RED + "            000000                   000000            000000               000000     000000")
+            print(colorama.Fore.RED + "            000000                   000000            000000              000000       000000")
+            print(colorama.Fore.RED + "            000000                   000000000000000000000000             000000000000000000000")
+            print(colorama.Fore.RED + "            000000                   000000000000000000000000            00000000000000000000000")
+            print(colorama.Fore.RED + "            000000                   000000000000000000000000           0000000000000000000000000")
+            print(colorama.Fore.RED + "            000000                   000000            000000          000000               000000")
+            print(colorama.Fore.RED + "            000000                   000000            000000         000000                 000000")
+            print(colorama.Fore.RED + "            000000                   000000            000000        000000                   000000")
+            print(colorama.Fore.RED + "            000000                   000000            000000       000000                     000000")
+            print(colorama.Fore.RED + "            000000                   000000            000000      000000                       000000")
+            print(colorama.Fore.RED + "")
+            print(colorama.Fore.RED + "")
             
-            print(colorama.Fore.GREEN)
-            cowsay.tux("hello im a abolfazl me github is @ABSAMADI")
-            print(colorama.Fore.RESET)
-            print(colorama.Fore.GREEN + '1.green ' + colorama.Fore.RED + '2.red ' + colorama.Fore.YELLOW + '3.yellow ' + colorama.Fore.BLUE + '4.blue ' + colorama.Fore.MAGENTA + '5.magenta ' + colorama.Fore.CYAN + '6.cyan ' + colorama.Fore.WHITE + '7.All colors not gey' + colorama.Fore.BLACK + " 8.no gey" + colorama.Fore.YELLOW + " 9.about me")
-            try:
-                a = int(input(colorama.Fore.LIGHTGREEN_EX + "Enter a number: "))
-            except:
-                print(colorama.Fore.RED + "error: enter a number")
 
-            if a == 1:
-                while 1:
-                    print(colorama.Fore.GREEN + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+            print(colorama.Fore.GREEN + "1.Heck_simulator")
+            print(colorama.Fore.GREEN + "2.sms spammer")
+            print(colorama.Fore.GREEN + "3.DDos attack")
+            print(colorama.Fore.GREEN + "4.Heck cctv")
+            print(colorama.Fore.RED + "")
+            print(colorama.Fore.RED + "")
 
-            if a == 2:
-                while 1:
-                    print(colorama.Fore.RED + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+            l = int(input(colorama.Fore.LIGHTGREEN_EX + "Enter a number: "))
 
-            if a == 3:
-                while 1:
-                    print(colorama.Fore.YELLOW + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+            if l == 1:
+                
+                os.system("cls")
 
-            if a == 4:
-                while 1:
-                    print(colorama.Fore.BLUE + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+                print(colorama.Fore.GREEN)
+                print(cowsay.get_output_string("dragon", "Hello, welcome to the Heck simulator! created by @ABSAMADI"))
+                print(colorama.Fore.RESET)
 
-            if a == 5:
-                while 1:
-                    print(colorama.Fore.MAGENTA + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+                print(colorama.Fore.GREEN + '1.green ' + colorama.Fore.RED + '2.red ' + colorama.Fore.YELLOW + '3.yellow ' + colorama.Fore.BLUE + '4.blue ' + colorama.Fore.MAGENTA + '5.magenta ' + colorama.Fore.CYAN + '6.cyan ' + colorama.Fore.WHITE + '7.All colors not gey' + colorama.Fore.BLACK + " 8.no gey" + colorama.Fore.YELLOW + " 9.about me")
+                try:
+                    a = int(input(colorama.Fore.LIGHTGREEN_EX + "Enter a number: "))
+                except:
+                    print(colorama.Fore.RED + "error: enter a number")
 
+                if a == 1:
+                    while 1:
+                        print(colorama.Fore.GREEN + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
 
-            if a == 6:
-                while 1:
-                    print(colorama.Fore.CYAN + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+                if a == 2:
+                    while 1:
+                        print(colorama.Fore.RED + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
 
+                if a == 3:
+                    while 1:
+                        print(colorama.Fore.YELLOW + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
 
-            if a == 7:
-                while 1:
-                    print(colorama.Fore.GREEN + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
-                    print(colorama.Fore.RED + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
-                    print(colorama.Fore.YELLOW + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
-                    print(colorama.Fore.BLUE + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
-                    print(colorama.Fore.MAGENTA + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
-                    print(colorama.Fore.CYAN + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+                if a == 4:
+                    while 1:
+                        print(colorama.Fore.BLUE + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
 
-            if a == 8:
-                while 1:
-                    print(colorama.Fore.BLACK + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)   
-                    print(colorama.Fore.WHITE + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)               
-
-            def about_me():
-                time.sleep(1)
-                print(colorama.Fore.YELLOW + "im name is abolfazl")
-                time.sleep(1)
-                print(colorama.Fore.YELLOW + "im 12 years old")
-                time.sleep(1)
-                print(colorama.Fore.YELLOW + "im iranian")
-                time.sleep(1)
-                print(colorama.Fore.YELLOW + "im python developer")
-                time.sleep(1)
-                print(colorama.Fore.YELLOW + "im github is @ABSAMADI")
-                time.sleep(1)
-                print(colorama.Fore.YELLOW + "ilove python")
-                time.sleep(1)
-                print(colorama.Fore.YELLOW + "ilove persian")
-                time.sleep(1)
-                print(colorama.Fore.RED + "good boy")
-                time.sleep(1)
-                print(colorama.Fore.GREEN + ".")
-                print(colorama.Fore.WHITE + ".")
-                print(colorama.Fore.RED + ".")
-                print(" ")
-                time.sleep(1)
-                print(colorama.Fore.GREEN + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001110100")
-                print(colorama.Fore.GREEN + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001010100")
-                print(colorama.Fore.GREEN + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001010100")
-                print(colorama.Fore.GREEN + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011100")
-                print(colorama.Fore.GREEN + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011000")
-                print(colorama.Fore.GREEN + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
-                print(colorama.Fore.WHITE + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
-                print(colorama.Fore.WHITE + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
-                print(colorama.Fore.WHITE + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
-                print(colorama.Fore.WHITE + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
-                print(colorama.Fore.WHITE + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
-                print(colorama.Fore.WHITE + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
-                print(colorama.Fore.RED + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
-                print(colorama.Fore.RED + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
-                print(colorama.Fore.RED + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
-                print(colorama.Fore.RED + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
-                print(colorama.Fore.RED + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
-                print(colorama.Fore.RED + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
+                if a == 5:
+                    while 1:
+                        print(colorama.Fore.MAGENTA + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
 
 
-            if a == 9:
-                about_me()
+                if a == 6:
+                    while 1:
+                        print(colorama.Fore.CYAN + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+
+
+                if a == 7:
+                    while 1:
+                        print(colorama.Fore.GREEN + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+                        print(colorama.Fore.RED + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+                        print(colorama.Fore.YELLOW + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+                        print(colorama.Fore.BLUE + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+                        print(colorama.Fore.MAGENTA + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+                        print(colorama.Fore.CYAN + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)
+
+                if a == 8:
+                    while 1:
+                        print(colorama.Fore.BLACK + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)   
+                        print(colorama.Fore.WHITE + str(random.randint(1, 7981111119580978570496516004489176876261892861444444824094769999999980)) + colorama.Fore.RESET)               
+
+                if a == 9:
+                    time.sleep(1)
+                    print(colorama.Fore.YELLOW + "im name is abolfazl")
+                    time.sleep(1)
+                    print(colorama.Fore.YELLOW + "im 12 years old")
+                    time.sleep(1)
+                    print(colorama.Fore.YELLOW + "im iranian")
+                    time.sleep(1)
+                    print(colorama.Fore.YELLOW + "im python developer")
+                    time.sleep(1)
+                    print(colorama.Fore.YELLOW + "im github is @ABSAMADI")
+                    time.sleep(1)
+                    print(colorama.Fore.YELLOW + "ilove python")
+                    time.sleep(1)
+                    print(colorama.Fore.YELLOW + "ilove persian")
+                    time.sleep(1)
+                    print(colorama.Fore.RED + "good boy")
+                    time.sleep(1)
+                    print(colorama.Fore.GREEN + ".")
+                    print(colorama.Fore.WHITE + ".")
+                    print(colorama.Fore.RED + ".")
+                    print(" ")
+                    time.sleep(1)
+                    print(colorama.Fore.GREEN + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001110100")
+                    print(colorama.Fore.GREEN + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001010100")
+                    print(colorama.Fore.GREEN + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001010100")
+                    print(colorama.Fore.GREEN + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011100")
+                    print(colorama.Fore.GREEN + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011000")
+                    print(colorama.Fore.GREEN + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
+                    print(colorama.Fore.WHITE + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
+                    print(colorama.Fore.WHITE + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
+                    print(colorama.Fore.WHITE + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
+                    print(colorama.Fore.WHITE + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
+                    print(colorama.Fore.WHITE + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
+                    print(colorama.Fore.WHITE + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
+                    print(colorama.Fore.RED + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
+                    print(colorama.Fore.RED + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
+                    print(colorama.Fore.RED + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
+                    print(colorama.Fore.RED + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
+                    print(colorama.Fore.RED + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
+                    print(colorama.Fore.RED + "0101010101010110011000111000111100010100110010110011101100101110001100001010111010101001011010")
+
+
+
 
 
 except:
